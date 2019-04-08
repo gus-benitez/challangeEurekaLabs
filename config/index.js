8989d9f8bce59b1b5512ce9d9153b93d3d53f5ee
Gustavo Benitez <ing.benitez.gustavo@gmail.com>
2019-04-08 01:52:13 -0300

    Deploy Heroku

--- config/index.js
+++ config/index.js
@@ -4,8 +4,10 @@ require('dotenv').config()
 const config = {
   port: process.env.PORT || 3000,
   apikey: process.env.APIKEY,
-  dbHost: process.env.DB_HOST,
-  dbPort: process.env.DB_PORT,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST || 'localhost',
+  dbPort: process.env.DB_PORT || 27017,
   dbName: process.env.DB_NAME,
   authJwtSecret: process.env.AUTH_JWT_SECRET
 }
